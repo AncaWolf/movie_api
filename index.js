b10606293abe31f95661e57e321227ffb96742af
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-10-05 14:19:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express'),
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
