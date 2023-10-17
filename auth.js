ee78844e343c4787e1ec58a0505c97955fbc65f1
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-10-17 21:47:07 +0200

    fixed unexpected token error

--- auth.js
+++ auth.js
@@ -7,25 +7,26 @@ require('./passport');
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
-        subject: user.Username,
+        subject: user.username,
         expiresIn: '7d',
-        algorithm: 'HS256'
+        algorithm: 'HS256',
     });
 }
 
 // POST login
 module.exports = (router) => {
-    router.post('./login', (req, res) => {
+    router.post('/login', (req, res) => {
+        // console.log('called');
         passport.authenticate('local', {session: false}, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
                     message: 'Something is not right',
-                    user: user
+                    user: user,
                 });
             }
             req.login(user, { session: false }, (error) => {
                 if (error) {
-                    res.send(error),
+                    res.send(error);
                 }
                 let token = generateJWTToken(user.toJSON());
                 return res.json({ user, token });
