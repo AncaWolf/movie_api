c0a54e1d80a7a70f227f317d1f7788d4e3a3dad8
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-10-18 17:38:52 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -16,7 +16,7 @@ let generateJWTToken = (user) => {
 // POST login
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        // console.log('called');
+        console.log('called');
         passport.authenticate('local', {session: false}, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
