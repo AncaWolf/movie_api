a56d8868999b66b74ed3dab5f6e859c7d48c59bd
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-10-18 21:23:32 +0200

    Update passport.js
    added password validation

--- passport.js
+++ passport.js
@@ -20,8 +20,12 @@ passport.use(
                 if(!user) {
                     console.log('incorrect username');
                     return callback(null, false, {
-                        message: 'Incorrect username of password.',
+                        message: 'Incorrect username or password.',
                     });
+                }
+                if (!user.validatePassword(password)) {
+                    console.log('incorrect password');
+                    return callback(null, false, {message: 'Incorrect password.'});
                 }
                 console.log('finished');
                 return callback(null, user);
