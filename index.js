ec0356f66d19cf3503baa004c862efb70687a505
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-11-05 11:10:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
 // GET - return list of all movies to the user
 app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find().then((movies) => {
-        res.status(201).json(movies);
+        res.status(200).json(movies);
     })
     .catch((err) => {
         console.error(err);
