9ac60a6d49fe441a6b766393322f301d66a8491c
AncaWolf <135062411+AncaWolf@users.noreply.github.com>
2023-11-14 19:19:40 +0100

    update auth/models/passport

--- models.js
+++ models.js
@@ -3,33 +3,33 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-    Title:{type: String, required: true},
-    Description: {type: String, required: true},
-    Genre: {
-        directorName: String,
-        Description: String
-    },
-    Director: {
-        directorName: String,
-        Bio: String
-    },
-    Actors: [String],
-    ImagePath: String,
-    Featured: Boolean
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    directorName: String,
+    Description: String
+  },
+  Director: {
+    directorName: String,
+    Bio: String
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birthdate: Date,
-    FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthdate: Date,
+  FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 userSchema.statics.hashPassword = (password) => {
-    return bcrypt.hashSync(password, 10);
+  return bcrypt.hashSync(password, 10);
 };
-userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Password);
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
